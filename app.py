53a835abf397a3655cab58b31e4b6faf2b4a491c
svscodes <vyaascode@gmail.com>
2022-10-12 21:12:11 +0530

    added  locations

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 app = Flask(__name__)
 
-@app.route('/xyz')
+topLocation = dict({"Delhi": ["laal kial","modi"], "Chennai": ["Beach","Parthasarathy Temple","hgfd"]})
+
+@app.route('/')
 def home():
    return render_template('index.html')
+
+@app.route('/agenda',methods=['POST'])
+def timetable():
+   result = request.form
+   print(result["location"])
+   print(topLocation[result["location"]])
+   chosenLocation = topLocation[result["location"]]
+   return render_template('agenda.html',chosenLocation = chosenLocation  )
+
+
+
+
 if __name__ == '__main__':
    app.run()
+
